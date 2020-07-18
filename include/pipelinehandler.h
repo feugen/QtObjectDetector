3f824461b7644941677a6270a68ac49c9c68ec6f
Eugen Fischer <eugen.fischer.android@googlemail.com>
2020-07-18 16:17:19 +0200

    Prepared some functions we want to implement

--- include/pipelinehandler.h
+++ include/pipelinehandler.h
@@ -10,9 +10,6 @@ class PipelineHandler : public QObject
 public:
     explicit PipelineHandler(QObject *parent = nullptr);
 
-
-
-
     std::vector<std::pair<cv::Mat, Base::e_OpenCVColorFormat>> &getImagePipeline();
     std::vector<std::function<void()>> &getFunctionPipeline();
     std::vector<std::pair<std::vector<std::function<void()>>, QString>> &getAvailablePipelines();
@@ -22,6 +19,17 @@ public:
     std::function<void (cv::InputArray, cv::OutputArray, double maxValue, int adaptiveMethod, int thresholdType, int blockSize, double C)> &getAdaptiveThreshold();
 
 private:
+    //Todo, parameter names
+    std::function<void(cv::InputArray, double, cv::InputArray, double, double, cv::OutputArray, int)> m_addWeighted = cv::addWeighted;
+    std::function<void(cv::InputArray, cv::OutputArray, int, int, double, double, int)> m_laplacian = cv::Laplacian;
+    std::function<void(cv::InputArray, cv::OutputArray, int, int, int, int, double, double, int)> m_sobel = cv::Sobel;
+    std::function<void(cv::InputArray, cv::OutputArray, cv::InputArray, cv::Point, int, int, const cv::Scalar&)> m_erode = cv::erode;
+    std::function<void(cv::InputArray, double, cv::OutputArray)> m_pow = static_cast<void(*)(cv::InputArray, double, cv::OutputArray)>(cv::pow);
+    std::function<void(cv::InputArray, cv::OutputArray, cv::InputArray)> m_bitwiseNot = cv::bitwise_not;
+    std::function<void(cv::InputArray, cv::OutputArray, cv::Size, double, double, int)> m_gaussianBlur = cv::GaussianBlur;
+    std::function<void(cv::InputArray, cv::OutputArray, cv::Size, cv::Point, int)> m_blur = cv::blur;
+    std::function<void(cv::InputArray, cv::OutputArray, int)> m_medianBlur = cv::medianBlur;
+
     std::function<void(cv::InputArray, cv::OutputArray, int, int)> m_cvtColor = cv::cvtColor;
     std::function<void(cv::InputArray, cv::OutputArray, double threshold, double max_value, int threshold_type)> m_threshold = cv::threshold;
     std::function<void(cv::InputArray, cv::OutputArray, double maxValue, int adaptiveMethod, int thresholdType, int blockSize, double C)> m_adaptiveThreshold = cv::adaptiveThreshold;
